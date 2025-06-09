90c4208b90e0bb695c428a5dcb652d373d52703a
Daniel P. Mgawi <pdmgawi@gmail.com>
2025-06-10 01:09:52 +0200

    chore: remove migration files and related database schema definitions

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250609_223634_init from './20250609_223634_init';
-
-export const migrations = [
-  {
-    up: migration_20250609_223634_init.up,
-    down: migration_20250609_223634_init.down,
-    name: '20250609_223634_init'
-  },
-];
